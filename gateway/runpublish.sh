d4e3bd64aef1867424b59af7ac6f68be4122e26c
Ulf Lilleengen <lulf@redhat.com>
2022-08-16 10:07:54 +0200

    Use letter B

--- gateway/runpublish.sh
+++ gateway/runpublish.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 MAC=DD:5E:B7:6F:AC:C7
-./target/release/ble-gateway -vvv -d ${MAC} --report-interval 10sec | while read -r line; do curl -X POST -H "Content-Type: application/json" -d "$line" -u 'microbit@ossummit22:hey-rodney' https://http.sandbox.drogue.cloud/v1/foo; done
+ble-gateway -vvv -d ${MAC} --report-interval 10sec | while read -r line; do curl -X POST -H "Content-Type: application/json" -d "$line" -u 'microbit@ossummit22:1234' https://http.sandbox.drogue.cloud/v1/foo; done
--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 MAC=DD:5E:B7:6F:AC:C7
-while true; do drgdfu upload ble-gatt --device ${MAC} cloud --http https://http.sandbox.drogue.cloud --application ossummit22 --device microbit --password 1234; done
+while true; do drgdfu -vvv upload ble-gatt --device ${MAC} cloud --http https://http.sandbox.drogue.cloud --application ossummit22 --device microbit --password 1234; done
