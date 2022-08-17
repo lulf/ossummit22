8800585851149e116baa6db2514d7ea187dfbaad
Ulf Lilleengen <lulf@redhat.com>
2022-08-17 09:40:27 +0200

    Change to B

--- firmware/application/src/main.rs
+++ firmware/application/src/main.rs
@@ -134,7 +134,7 @@ async fn main(s: Spawner, p: Peripherals) {
     display.set_brightness(Brightness::MAX);
     loop {
         let _ = display
-            .display('A'.to_frame(), Duration::from_secs(1))
+            .display('B'.to_frame(), Duration::from_secs(1))
             .await;
         Timer::after(Duration::from_secs(1)).await;
     }
